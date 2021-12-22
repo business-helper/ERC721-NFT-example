74498fb44b1d2a648c8902b2650fb29128359162
Bi Wu <alerk_star@outlook.com>
2021-12-23 00:50:12 +0800

    added mint;

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.0;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+// import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import './ERC721Full.sol';
 
-contract Color is ERC721 {
-  address public admin;
+contract Color is ERC721Full {
+  // address public admin;
   string[] public colors;
   mapping(string => bool) _colorExists;
 
-  constructor() ERC721("Color", "COLOR") {
-    admin = msg.sender;
-  }
+  constructor() ERC721Full("Color", "COLOR") public {}
 
   function mint(string memory _color) public {
     // require unique color
     // color - add it
     // call the mint function
     // Color - track it.
-    uint id = colors.push(_color);
+    require(!_colorExists[_color]);
+    uint _id = colors.push(_color);
     _mint(msg.sender, _id);
     _colorExists[_color] = true;
-    
   }
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -25,12 +25,26 @@ contract('Color', (accounts) => {
 
     it('has a name', async () => {
       const name = await contract.name();
-      asset.equal(name, 'Color');
+      assert.equal(name, 'Color');
     })
 
     it('has a symbol', async () => {
       const symbol = await contract.symbol();
       assert.equal(symbol, 'COLOR');
+    })
+  })
+
+  describe('minting', async () => {
+    it('creates a new  token', async () => {
+      const result = await contract.mint('#EC058E');
+      const totalSupply = await contract.totalSupply();
+      // SUCCESS
+      assert.equal(totalSupply, 1);
+
+      const event = result.logs[0].args;
+      assert.equal(event.tokenId.toNumber(), 1, 'id is correct');
+      assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct');
+      assert.equal(event.to, accounts[0], 'to is correct');
     })
   })
 
