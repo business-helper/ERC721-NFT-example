38ab2628edb46d85cbdc2b1f8bf184227c4629ae
Bi Wu <alerk_star@outlook.com>
2021-12-23 00:06:24 +0800

    compiled & generated abi files;

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,11 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 
 contract Color is ERC721 {
-  constructor() ERC721("Color", "COLOR") public {
+  address public admin;
+  string[] public colors;
+  mapping(string => bool) _colorExists;
 
+  constructor() ERC721("Color", "COLOR") {
+    admin = msg.sender;
   }
 
-  
+  function mint(string memory _color) public {
+    // require unique color
+    // color - add it
+    // call the mint function
+    // Color - track it.
+    uint id = colors.push(_color);
+    _mint(msg.sender, _id);
+    _colorExists[_color] = true;
+    
+  }
 }
